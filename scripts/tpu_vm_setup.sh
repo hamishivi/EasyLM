9ff976732672dfffa2655d0ade73899a003b5a5a
Jacob Morrison <jacob1morrison@gmail.com>
2024-05-14 14:46:09 -0700

    Update tpu_vm_setup.sh

--- scripts/tpu_vm_setup.sh
+++ scripts/tpu_vm_setup.sh
@@ -9,7 +9,8 @@ sudo apt-get update && sudo apt-get install -y \
     nodejs \
     bmon \
     p7zip-full \
-    nfs-common
+    nfs-common \
+    golang # needed for jax-smi
 
 
 # Python dependencies
@@ -26,8 +27,8 @@ einops
 --extra-index-url https://download.pytorch.org/whl/cpu
 torch==2.0.1
 transformers==4.31.0
-datasets==2.16.1
-huggingface_hub==0.19.4
+datasets==2.14.2
+huggingface_hub==0.16.4
 tqdm
 h5py
 ml_collections
@@ -42,6 +43,7 @@ pydantic
 fastapi
 uvicorn
 gradio
+jax-smi
 EndOfFile
 
 pip install --upgrade -r $HOME/tpu_requirements.txt
